2b694d774326a1ea748132ce39a818e6451d976d
Uzma Syed <uzma.syed@safetyculture.io>
2017-01-07 15:48:10 +1100

    managed to get callback of yelp wpi working

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
-/**
- * Created by uzmasyed on 1/01/2017.
- */
-/**
- * Generates a random number and returns it as a string for OAuthentication
- * @return {string}
- */
+var settings;
+var parameters;
+
+
 function nonce_generate() {
-  //console.log("I am in nonce_generate")
   return (Math.floor(Math.random() * 1e12).toString());
 }
 
 
 function getYelpReviews(place, city) {
   var location = place.name;
-  //var yelp_url = 'https://api.yelp.com/v2/search?' + 'term=Medina&location=Kitsilano'
   var yelp_url = 'https://api.yelp.com/v2/search?'
   var YELP_KEY_SECRET = 'rML1MhFwm6phKTKPrwpSbnZ8fss';
   var YELP_TOKEN_SECRET = 'ej_V-eaYPaAfXblnM9OshO8erRE';
@@ -33,30 +28,23 @@ function getYelpReviews(place, city) {
   var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, YELP_KEY_SECRET, YELP_TOKEN_SECRET);
   parameters.oauth_signature = encodedSignature;
 
-  var settings = {
+
+  settings = {
     url: yelp_url,
     data: parameters,
     cache: true,
     dataType: 'jsonp',
     success: function(results) {
-      // Do stuff with results
-      //console.log("I have received results successfully")
-      //console.log("result received is" + results.businesses[0].snippet_text);
 
-      console.log(results);
-        //return results;
-
-      // set infowindow content
-      // open infowindow
+      console.log("results in success function is" + results)
+      //console.log("snippet_text in results in success function is" +results.businesses[0].snippet_text);
 
     },
     fail: function() {
-      // Do stuff on fail
       alert("Problem occurred");
     }
   };
 
-  // Send AJAX query via jQuery library.
-  $.ajax(settings);
-
+  var resultFromAjaxRequest = $.ajax(settings);
+  return resultFromAjaxRequest;
 }
